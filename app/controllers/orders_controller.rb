a8e597ad37e2a6813b1c0a8df3dc2ee571bfe6d2
markdoble <mark.doble@queensu.ca>
2016-06-19 16:52:09 -0400

    added user authentication to orders controller before security fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   layout "order_layout"
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
 
 
