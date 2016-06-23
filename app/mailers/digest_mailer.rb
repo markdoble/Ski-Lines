499dbe11999e7c26738e5e636f6d0eb54c99bdce
markdoble <mark.doble@queensu.ca>
2016-06-23 10:43:59 -0400

    finished sendgrid integration

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -1,5 +1,5 @@
 class DigestMailer < ActionMailer::Base
-  default from: "digest@ski-lines.com"
+  default from: "no-reply@ski-lines.com"
 
   def weekly_digest(subscriber, article_ids, subject)
     @url = 'https://www.ski-lines.com'
@@ -11,27 +11,16 @@ class DigestMailer < ActionMailer::Base
     end
     @articles = articles_arr
 
-    delivery_options = { user_name: 'digest@ski-lines.com',
-                         password: ENV["EMAIL_PASS"],
-                         address: "smtp.zoho.com"}
 
      mail(to: @subscriber.email,
-          subject: subject,
-          delivery_method_options: delivery_options)
+          subject: subject)
   end
 
   def welcome_mail(subscriber_id)
-    @url = 'http://www.ski-lines.com'
-
+    @url = 'https://www.ski-lines.com'
     @subscriber = EmailDigest.find_by_id(subscriber_id)
-
-    delivery_options = { user_name: 'digest@ski-lines.com',
-                         password: ENV["EMAIL_PASS"],
-                         address: "smtp.zoho.com"}
-
      mail(to: @subscriber.email,
-          subject: "Welcome to the Ski-Lines Email Digest",
-          delivery_method_options: delivery_options)
+          subject: "Welcome to the Ski-Lines Email Digest")
   end
 
 end
