94d3e261d8e69e346eac6ab726b52376eb83b85e
maxtrottier <maxtrottier@gmail.com>
2016-06-25 19:47:33 -0400

    Code Documentation
    Documentation and proper alignment of code.
    
    Development:
    -No functional changes were made.
    -Added documentation and fixed alignment.
    
    QA Testing:
    -Not required. No functional changes were made.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,29 +1,18 @@
+/*
+ * Function to be executed once the DOM has been fully loaded and ready
+ */
 $(document).ready(function(){
-	
-  
-	
-	
-	
-// All Product Filter Toggles
-  
-$(document).on('click', ".clothing_filter_button", function() {
-	 $(this).next(".clothing_filter_options").slideToggle();	 
+	// Define the click actions for all product filter toggles
+	$(document).on('click', '.clothing_filter_button', function() {
+		$(this).next('.clothing_filter_options').slideToggle();	 
+	});
+	$(document).on('click', '.hg_filter_button', function() {
+		 $(this).next('.hg_filter_options').slideToggle();	 
+	});
+	$(document).on('click', '.wax_filter_button', function() {
+		 $(this).next(".wax_filter_options").slideToggle();	 
+	});
+	$(document).on('click', '.acc_filter_button', function() {
+		 $(this).next(".acc_filter_options").slideToggle();	 
+	}); 
 });
-$(document).on('click', ".hg_filter_button", function() {
-	 $(this).next(".hg_filter_options").slideToggle();	 
-});
-$(document).on('click', ".wax_filter_button", function() {
-	 $(this).next(".wax_filter_options").slideToggle();	 
-});
-$(document).on('click', ".acc_filter_button", function() {
-	 $(this).next(".acc_filter_options").slideToggle();	 
-}); 
-
-
-
-
-
-
-
-});
-	
