ca9cef72ffc01aa42274830cef67f8e575158c4d
markdoble <mark.doble@queensu.ca>
2016-06-29 11:13:20 -0400

    wrote logic for order amount validation. still needs testing and completing implementation

--- app/models/concerns/order_methods.rb
+++ app/models/concerns/order_methods.rb
@@ -16,92 +16,78 @@ module OrderMethods
 
 
   module ClassMethods
- #These are class methods
+  #These are class methods to be called on an instance of the class, ex: Order.example(order)
 
-    def self.create_merchant_order_units(order)
-      # make array of order units and find its:
-        # shipping method
-        # shipping cost
-        # tax rate applied to the particular unit
-        # total cost of that unit (sum of product price, shipping cost, tax )
-      # add up total cost for all units
-      order_units_array = []
-      order.order_units.where.not(quantity: 0).each do |f|
-        order_units_array << f.unit
-      end
 
-      order_units_array.each do |f|
-        tax_rate += 0
-        per_product_tax(order)
-        (f.unit.product.shipping_charge + f.unit.product.price)*(1 + tax_rate)
-      end
-
-    end
 
 
   end
 
 
 
-  # Instance methods are not contained in the module: use on an instance of the class, such as @order
-    def per_product_tax(order)
-      if ["New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", 'Northwest Territories', 'Nunavut', 'Yukon'].include? order.prov_state
-          if order.prov_state == "New Brunswick" then @tax_rate = 0.13
-          elsif order.prov_state == "Newfoundland and Labrador" then @tax_rate = 0.13
-          elsif order.prov_state == "Nova Scotia" then @tax_rate = 0.15
-          elsif order.prov_state == "Ontario" then @tax_rate = 0.13
-          elsif order.prov_state == "Prince Edward Island" then @tax_rate = 0.14
-          elsif ['Northwest Territories', 'Nunavut', 'Yukon'].includes? order.prov_state then @tax_rate = 0.05
-          end
-      elsif order.prov_state == user.state_prov
-        if order.prov_state == "Alberta" then @tax_rate = 0.05
-        elsif order.prov_state == "British Columbia" then @tax_rate = 0.12
-        elsif order.prov_state == "Manitoba" then @tax_rate = 0.13
-        elsif order.prov_state == "Quebec" then @tax_rate = 0.1475
-        elsif order.prov_state == "Saskatchewan" then @tax_rate = 0.1
-        end
-      elsif order.prov_state != user.state_prov
-        if order.prov_state == "Alberta" then @tax_rate = 0.05
-        elsif order.prov_state == "British Columbia" then @tax_rate = 0.05
-        elsif order.prov_state == "Manitoba" then @tax_rate = 0.05
-        elsif order.prov_state == "Quebec" then @tax_rate = 0.05
-        elsif order.prov_state == "Saskatchewan" then @tax_rate = 0.05
-        end
-      elsif order.country != "Canada" then @tax_rate = 0
+    # Instance methods are not contained in the module: use on an instance of the class, such as @order
+    def calculate_order_amount(order)
+      order_units_array = []
+      order.order_units.where.not(quantity: 0).each do |f|
+        order_units_array << f.unit
       end
-      @tax_rate
-    end
-
-    def tax_rate_calc(order, user)
-          if ["New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", 'Northwest Territories', 'Nunavut', 'Yukon'].include? order.prov_state
-              if order.prov_state == "New Brunswick" then @tax_rate = 0.13
-              elsif order.prov_state == "Newfoundland and Labrador" then @tax_rate = 0.13
-              elsif order.prov_state == "Nova Scotia" then @tax_rate = 0.15
-              elsif order.prov_state == "Ontario" then @tax_rate = 0.13
-              elsif order.prov_state == "Prince Edward Island" then @tax_rate = 0.14
-              elsif ['Northwest Territories', 'Nunavut', 'Yukon'].includes? order.prov_state then @tax_rate = 0.05
-              end
-          elsif order.prov_state == user.state_prov
-            if order.prov_state == "Alberta" then @tax_rate = 0.05
-            elsif order.prov_state == "British Columbia" then @tax_rate = 0.12
-            elsif order.prov_state == "Manitoba" then @tax_rate = 0.13
-            elsif order.prov_state == "Quebec" then @tax_rate = 0.1475
-            elsif order.prov_state == "Saskatchewan" then @tax_rate = 0.1
-            end
-          elsif order.prov_state != user.state_prov
-            if order.prov_state == "Alberta" then @tax_rate = 0.05
-            elsif order.prov_state == "British Columbia" then @tax_rate = 0.05
-            elsif order.prov_state == "Manitoba" then @tax_rate = 0.05
-            elsif order.prov_state == "Quebec" then @tax_rate = 0.05
-            elsif order.prov_state == "Saskatchewan" then @tax_rate = 0.05
-            end
-          elsif order.country != "Canada" then @tax_rate = 0
+      test_amount = 0
+      order.merchant_orders.each do |f|
+        tax_rate_calc(f)
+        international_shipping_factor(f)
+        order_units_array.each do |ff|
+          if f.product_id == ff.product_id
+            test_amount += (ff.product.price + (ff.product.shipping_charge*shipping_factor))*tax_rate
+          else
+            next
           end
-          @tax_rate
+        end
+      end
     end
 
+    def international_shipping_factor(f)
+      case f.order.country
+      when "United States of America"
+        shipping_factor = 1.5
+      else
+        shipping_factor = 1
+      end
+    end
 
-
-
+    def tax_rate_calc(f)
+      merchant_location = f.user.state_prov
+      if f.shipping_method == "In Store Pick-Up"
+        customer_location = f.user.state_prov
+        customer_country = f.user.country
+      else
+        customer_location = f.order.prov_state
+        customer_country = f.order.country
+      end
+      if ["New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", 'Northwest Territories', 'Nunavut', 'Yukon'].include? customer_location
+        if customer_location == "New Brunswick" then tax_rate = 0.13
+        elsif customer_location == "Newfoundland and Labrador" then tax_rate = 0.13
+        elsif customer_location == "Nova Scotia" then tax_rate = 0.15
+        elsif customer_location == "Ontario" then tax_rate = 0.13
+        elsif customer_location == "Prince Edward Island" then tax_rate = 0.14
+        elsif ['Northwest Territories', 'Nunavut', 'Yukon'].includes? customer_location then tax_rate = 0.05
+        end
+      elsif customer_location == merchant_location
+        if customer_location == "Alberta" then tax_rate = 0.05
+        elsif customer_location == "British Columbia" then tax_rate = 0.12
+        elsif customer_location == "Manitoba" then tax_rate = 0.13
+        elsif customer_location == "Quebec" then tax_rate = 0.1475
+        elsif customer_location == "Saskatchewan" then tax_rate = 0.1
+        end
+      elsif customer_location != merchant_location
+        if customer_location == "Alberta" then tax_rate = 0.05
+        elsif customer_location == "British Columbia" then tax_rate = 0.05
+        elsif customer_location == "Manitoba" then tax_rate = 0.05
+        elsif customer_location == "Quebec" then tax_rate = 0.05
+        elsif customer_location == "Saskatchewan" then tax_rate = 0.05
+        end
+      elsif customer_country != "Canada" then tax_rate = 0
+      end
+      tax_rate
+    end
 
 end
