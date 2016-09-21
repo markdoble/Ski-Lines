c2024a5d2164c3147edd4f9436b2ef4512432000
markdoble <mark.doble@queensu.ca>
2016-09-20 20:14:48 -0400

    added merchant navbar

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -38,6 +38,13 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	// hide and show the dashboard navigation
+	$('.merchant_navigation').hide();
+	$('.dashboard_button').click(function(){
+			$('.merchant_navigation').toggle();
+	});
+
 });
 
 /*
