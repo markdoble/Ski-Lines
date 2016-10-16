12dba78720a018b7c5bee8e4cbe7e2c7c3108c2b
markdoble <mark.doble@queensu.ca>
2016-10-16 11:06:31 -0400

    fixed product layout

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module SkiSite
     config.active_job.queue_adapter = :sidekiq
     config.assets.precompile = config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
     config.assets.precompile += %w( my_js )
-    config.serve_static_assets = true
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
