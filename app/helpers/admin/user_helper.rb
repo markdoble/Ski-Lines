754bf34acb1d9c1725da3e21b338dcd8f6e2af60
markdoble <mark.doble@queensu.ca>
2016-11-05 14:58:26 -0400

    fixed merchant inventory calculation

--- app/helpers/admin/user_helper.rb
+++ app/helpers/admin/user_helper.rb
@@ -4,7 +4,9 @@ module Admin::UserHelper
     user = User.find_by_id(user)
     usd_value = 0
     user.products.where(status: true).each do |f|
-      usd_value += f.usd_price
+      f.units.each do |ff|
+        usd_value += ff.quantity*f.usd_price
+      end
     end
     number_to_currency(usd_value, unit: "$")
   end
@@ -13,7 +15,9 @@ module Admin::UserHelper
     user = User.find_by_id(user)
     cad_value = 0
     user.products.where(status: true).each do |f|
-      cad_value += f.cad_price
+      f.units.each do |ff|
+        cad_value += ff.quantity*f.cad_price
+      end
     end
     number_to_currency(cad_value, unit: "$")
   end
