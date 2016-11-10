04266bc905d05b8f0a860cdde535e6e678f272ed
markdoble <mark.doble@queensu.ca>
2016-11-09 20:46:14 -0500

    environment changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
 
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -39,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.delivery_method = :smtp
-  
+
 
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
