02bce0740300abe079df074db76a23286b74652a
markdoble <mark.doble@queensu.ca>
2016-11-26 15:24:39 -0500

    added scrape for swix...work in progress

---
+++ db/seeds/craft_scrape.rb
@@ -0,0 +1,330 @@
+require 'open-uri'
+require 'nokogiri'
+
+=begin
+# men's nordic tops
+begin
+  url = "http://shop.craftsports.us/men/nordic-ski.html?cat=98&limit=36"
+  doc = Nokogiri::HTML(open(url))
+rescue OpenURI::HTTPError => e
+else
+    doc.css(".products-grid .item").each do |i|
+      begin
+        location = i.at_css(".product-info .product-name a")[:href]
+        name = i.at_css(".product-info .product-name a").content
+        image = Nokogiri::HTML(open(location)).at_css(".left-content .product-image-gallery .gallery-image")[:src]
+        description = Nokogiri::HTML(open(location)).at_css(".left-content .stinno-proddetails").content
+        sku = Nokogiri::HTML(open(location)).at_css(".stinno-prodsku").content.tr("SKU#", "").strip
+        usd_msrp = i.at_css(".product-info .price-box .regular-price .price").content.tr("$", "")
+      rescue
+        next
+      end
+    next unless name && sku && image && description && location && usd_msrp
+
+    stock_product = Stockproduct.find_or_create_by(sku: sku) do |product|
+      product.description = description.strip
+      product.name = name
+      product.brand = "Craft"
+      product.ca_status = false
+      product.us_status = false
+      product.usd_msrp = usd_msrp
+      end
+
+      parent_category = Category.find_by_name("Men's Clothing")
+      product_category = Category.where(parent_id: parent_category.id, name: "Midlayers").first
+      StockproductCategory.create(
+        stockproduct_id: stock_product.id,
+        category_id: product_category.id
+      )
+
+    Stockphoto.create(
+      photo: open(image),
+      stockproduct_id: stock_product.id
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Small',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Medium',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'X-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'XX-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    end
+end
+
+
+
+# men's tights/pants
+begin
+  url = "http://shop.craftsports.us/men/nordic-ski.html?cat=100&limit=36"
+  doc = Nokogiri::HTML(open(url))
+rescue OpenURI::HTTPError => e
+else
+    doc.css(".products-grid .item").each do |i|
+      begin
+        location = i.at_css(".product-info .product-name a")[:href]
+        name = i.at_css(".product-info .product-name a").content
+        image = Nokogiri::HTML(open(location)).at_css(".left-content .product-image-gallery .gallery-image")[:src]
+        description = Nokogiri::HTML(open(location)).at_css(".left-content .stinno-proddetails").content
+        sku = Nokogiri::HTML(open(location)).at_css(".stinno-prodsku").content.tr("SKU#", "").strip
+        usd_msrp = i.at_css(".product-info .price-box .regular-price .price").content.tr("$", "")
+      rescue
+        next
+      end
+    next unless name && sku && image && description && location && usd_msrp
+
+    stock_product = Stockproduct.find_or_create_by(sku: sku) do |product|
+      product.description = description.strip
+      product.name = name
+      product.brand = "Craft"
+      product.ca_status = false
+      product.us_status = false
+      product.usd_msrp = usd_msrp
+      end
+
+      parent_category = Category.find_by_name("Men's Clothing")
+      if stock_product.name.downcase.include?("pant")
+        product_category = Category.where(parent_id: parent_category.id, name: "Training Pants").first
+      elsif stock_product.name.downcase.include?("tight")
+        product_category = Category.where(parent_id: parent_category.id, name: "Tights").first
+      end
+
+      StockproductCategory.create(
+        stockproduct_id: stock_product.id,
+        category_id: product_category.id
+      )
+
+    Stockphoto.create(
+      photo: open(image),
+      stockproduct_id: stock_product.id
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Small',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Medium',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'X-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'XX-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    end
+end
+
+# men's tights/pants
+begin
+  url = "http://shop.craftsports.us/men/nordic-ski.html?cat=102&limit=36"
+  doc = Nokogiri::HTML(open(url))
+rescue OpenURI::HTTPError => e
+else
+    doc.css(".products-grid .item").each do |i|
+      begin
+        location = i.at_css(".product-info .product-name a")[:href]
+        name = i.at_css(".product-info .product-name a").content
+        image = Nokogiri::HTML(open(location)).at_css(".left-content .product-image-gallery .gallery-image")[:src]
+        description = Nokogiri::HTML(open(location)).at_css(".left-content .stinno-proddetails").content
+        sku = Nokogiri::HTML(open(location)).at_css(".stinno-prodsku").content.tr("SKU#", "").strip
+        usd_msrp = i.at_css(".product-info .price-box .regular-price .price").content.tr("$", "")
+      rescue
+        next
+      end
+    next unless name && sku && image && description && location && usd_msrp
+
+    stock_product = Stockproduct.find_or_create_by(sku: sku) do |product|
+      product.description = description.strip
+      product.name = name
+      product.brand = "Craft"
+      product.ca_status = false
+      product.us_status = false
+      product.usd_msrp = usd_msrp
+      end
+
+      parent_category = Category.find_by_name("Men's Clothing")
+      if stock_product.name.downcase.include?("vest")
+        product_category = Category.where(parent_id: parent_category.id, name: "Training Vest").first
+      else
+        product_category = Category.where(parent_id: parent_category.id, name: "Training Jacket").first
+      end
+    StockproductCategory.create(
+      stockproduct_id: stock_product.id,
+      category_id: product_category.id
+    )
+
+    Stockphoto.create(
+      photo: open(image),
+      stockproduct_id: stock_product.id
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Small',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Medium',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'X-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'XX-Large',
+      quantity: 1,
+      colour: 'n/a'
+    )
+    end
+end
+=end
+
+
+# hats
+begin
+  url = "http://shop.craftsports.us/men/nordic-ski.html?cat=108&limit=36"
+  doc = Nokogiri::HTML(open(url))
+rescue OpenURI::HTTPError => e
+else
+    doc.css(".products-grid .item").each do |i|
+      begin
+        location = i.at_css(".product-info .product-name a")[:href]
+        name = i.at_css(".product-info .product-name a").content
+        image = Nokogiri::HTML(open(location)).at_css(".left-content .product-image-gallery .gallery-image")[:src]
+        description = Nokogiri::HTML(open(location)).at_css(".left-content .stinno-proddetails").content
+        sku = Nokogiri::HTML(open(location)).at_css(".stinno-prodsku").content.tr("SKU#", "").strip
+        usd_msrp = i.at_css(".product-info .price-box .regular-price .price").content.tr("$", "")
+      rescue
+        next
+      end
+    next unless name && sku && image && description && location && usd_msrp
+
+    stock_product = Stockproduct.find_or_create_by(sku: sku) do |product|
+      product.description = description.strip
+      product.name = name
+      product.brand = "Craft"
+      product.ca_status = false
+      product.us_status = false
+      product.usd_msrp = usd_msrp
+      end
+
+
+    product_category = Category.where(name: "Hats and Toques").first
+    StockproductCategory.create(
+      stockproduct_id: stock_product.id,
+      category_id: product_category.id
+    )
+
+    Stockphoto.create(
+      photo: open(image),
+      stockproduct_id: stock_product.id
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Small',
+      quantity: 1,
+      colour: 'n/a'
+    )
+
+    end
+end
+
+# gloves
+begin
+  url = "http://shop.craftsports.us/men/nordic-ski.html?cat=110&limit=36"
+  doc = Nokogiri::HTML(open(url))
+rescue OpenURI::HTTPError => e
+else
+    doc.css(".products-grid .item").each do |i|
+      begin
+        location = i.at_css(".product-info .product-name a")[:href]
+        name = i.at_css(".product-info .product-name a").content
+        image = Nokogiri::HTML(open(location)).at_css(".left-content .product-image-gallery .gallery-image")[:src]
+        description = Nokogiri::HTML(open(location)).at_css(".left-content .stinno-proddetails").content
+        sku = Nokogiri::HTML(open(location)).at_css(".stinno-prodsku").content.tr("SKU#", "").strip
+        usd_msrp = i.at_css(".product-info .price-box .regular-price .price").content.tr("$", "")
+      rescue
+        next
+      end
+    next unless name && sku && image && description && location && usd_msrp
+
+    stock_product = Stockproduct.find_or_create_by(sku: sku) do |product|
+      product.description = description.strip
+      product.name = name
+      product.brand = "Craft"
+      product.ca_status = false
+      product.us_status = false
+      product.usd_msrp = usd_msrp
+      end
+
+
+    product_category = Category.where(name: "Gloves & Mitts").first
+    StockproductCategory.create(
+      stockproduct_id: stock_product.id,
+      category_id: product_category.id
+    )
+
+    Stockphoto.create(
+      photo: open(image),
+      stockproduct_id: stock_product.id
+    )
+    Stockunit.create(
+      stockproduct_id: stock_product.id,
+      size: 'Small',
+      quantity: 1,
+      colour: 'n/a'
+    )
+
+    end
+end
